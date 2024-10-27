e388cd4c55107034d5921ef2bc968376183f21eb
Henrique Santana <hsantana.sousass@gmail.com>
2024-10-27 16:58:17 -0300

    fix: review has_many association in singular

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :advertisements, dependent: :destroy
   has_many :messages, dependent: :nullify
   has_many :transactions, dependent: :nullify
-  has_many :review, dependent: :nullify
+  has_many :reviews, dependent: :nullify
 end
