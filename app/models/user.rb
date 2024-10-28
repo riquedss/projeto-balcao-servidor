092030b7bfd45da1f6203a607ceef1a92710ba55
Henrique Santana <hsantana.sousass@gmail.com>
2024-10-27 23:47:42 -0300

    refactor: actions to be done with objects associated with the user when it is destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :advertisements, dependent: :destroy
-  has_many :messages, dependent: :nullify
-  has_many :negotiations, dependent: :nullify
-  has_many :reviews, dependent: :nullify
+  has_many :messages, dependent: :restrict_with_exception
+  has_many :negotiations, dependent: :restrict_with_exception
+  has_many :reviews, dependent: :destroy
 end
