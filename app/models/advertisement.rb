c516eacf209bde738ac17eb96cc745da31b6686b
Henrique Santana <hsantana.sousass@gmail.com>
2024-10-28 23:02:22 -0300

    test: covers advertisement relationship rules

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,5 +1,5 @@
 class Advertisement < ApplicationRecord
   belongs_to :user
   has_many :negotiations, dependent: :restrict_with_exception
-  has_many :messages, dependent: :destroy
+  has_many :messages, dependent: :restrict_with_exception
 end
