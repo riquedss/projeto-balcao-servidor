dd652792d7085e1a5f7bfbfd47b818cf0e25e91a
Henrique Santana <hsantana.sousass@gmail.com>
2024-11-20 02:22:54 -0300

    feat: checking logged in user and handling requests with denied access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :authenticate_api_v1_user!, unless: :public_action?
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def current_user
+    current_api_v1_user
+  end
+
+  rescue_from CanCan::AccessDenied do
+    head :forbidden
+  end
 
   protected
 
@@ -8,5 +17,9 @@ class ApplicationController < ActionController::API
     allowed_parameters = %i[full_name]
     devise_parameter_sanitizer.permit(:sign_up, keys: allowed_parameters)
     devise_parameter_sanitizer.permit(:account_update, keys: allowed_parameters)
+  end
+
+  def public_action?
+    devise_controller? && %w[create sign_in].include?(action_name)
   end
 end
