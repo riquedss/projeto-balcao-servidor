18ba697878a8205c7750348702356dcf063ea920
gustavo-ferreira03 <gustavo_cosme@id.uff.br>
2024-12-19 17:03:56 -0300

    test: fix negotiation specs

--- spec/models/negotiation_spec.rb
+++ spec/models/negotiation_spec.rb
@@ -2,52 +2,75 @@ require 'rails_helper'
 
 RSpec.describe Negotiation, type: :model do
   describe "Relationship Rules:" do
-    let(:anunciador) { create(:user) }
+    let(:advertisement_owner) { create(:user) }
     let(:interested) { create(:user, :cpf_alternativo) }
-    let(:advertisement) { create(:advertisement, user: anunciador) }
-    let(:negotiation) { create(:negotiation, user: anunciador, advertisement: advertisement) }
+    let(:advertisement) { create(:advertisement, user: advertisement_owner) }
 
-    context "When creating an Negotiation in the database," do
+    context "When creating a Negotiation in the database," do
       it "must be possible to associate the Negotiation with 1 User." do
-        negotiation1 = create(:negotiation, user: interested, advertisement: advertisement)
-
-        expect(interested.id).to eq(negotiation1.user.id)
+        negotiation = create(:negotiation, user: interested, advertisement: advertisement)
+        expect(negotiation.user).to eq(interested)
       end
 
       it "must be possible to associate the Negotiation with 1 Advertisement." do
-        negotiation1 = create(:negotiation, user: interested, advertisement: advertisement)
+        negotiation = create(:negotiation, user: interested, advertisement: advertisement)
+        expect(negotiation.advertisement).to eq(advertisement)
+      end
 
-        expect(advertisement.id).to eq(negotiation1.advertisement.id)
+      it "must not allow a user to negotiate their own advertisement" do
+        expect {
+          create(:negotiation, user: advertisement_owner, advertisement: advertisement)
+        }.to raise_error(ActiveRecord::RecordInvalid, /You cannot negotiate with your own advertisement./)
       end
 
       it "must be mandatory to have an associated User model object" do
-        expect { create(:negotiation) }.to raise_error(/User must exist/)
+        expect {
+          create(:negotiation, user: nil, advertisement: advertisement)
+        }.to raise_error(ActiveRecord::RecordInvalid, /User must exist/)
       end
 
       it "must be mandatory to have an associated Advertisement model object" do
-        expect { create(:negotiation) }.to raise_error(/Advertisement must exist/)
+        expect {
+          create(:negotiation, user: interested, advertisement: nil)
+        }.to raise_error(ActiveRecord::RecordInvalid, /Advertisement must exist/)
       end
     end
 
-    context "When persisting a Negotiation in the database," do
-      it "must be possible to associate the user with N Reviews." do
-        review1 = create(:review, user: interested, negotiation: negotiation)
-        review2 = create(:review, user: interested, negotiation: negotiation)
-        review3 = create(:review, user: interested, negotiation: negotiation)
+    context "When updating a Negotiation," do
+      let(:negotiation) { create(:negotiation, user: interested, advertisement: advertisement) }
 
-        associated_objects = negotiation.reviews
+      it "must allow confirmation" do
+        negotiation.update!(status: :pending)
+        expect {
+          negotiation.confirm!
+        }.to change { negotiation.reload.status }.from("pending").to("confirmed")
+      end
+    end
 
-        expect([ review1.id, review2.id, review3.id ]).to match_array(associated_objects.ids)
+    context "When completing a Negotiation," do
+      let(:negotiation) { create(:negotiation, user: interested, advertisement: advertisement, status: :confirmed) }
+
+      it "must allow completion if the negotiation is confirmed" do
+        expect {
+          negotiation.complete!
+        }.to change { negotiation.reload.status }.from("confirmed").to("completed")
+      end
+
+      it "must not allow completion if the negotiation is not confirmed" do
+        negotiation.update!(status: :pending)
+        expect {
+          negotiation.complete!
+        }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end
 
     context "When deleting a Negotiation from the database," do
+      let(:negotiation) { create(:negotiation, user: interested, advertisement: advertisement) }
+
       it "all associated objects must be deleted if they are of the Review model." do
-        review1 = create(:review, user: interested, negotiation: negotiation)
-
+        review = create(:review, user: interested, negotiation: negotiation)
         negotiation.destroy
-
-        expect { Review.find(review1.id) }.to raise_error(ActiveRecord::RecordNotFound)
+        expect { Review.find(review.id) }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
